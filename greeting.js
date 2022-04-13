b053694d1d6b8940067f5db927829c44291f038c
Bekbolsun <linex1827@gmail.com>
2022-04-13 19:36:48 +0600

    First commit

--- greeting.js
+++ greeting.js
@@ -1,22 +1,75 @@
-let  obj = "*"
-<<<<<<< HEAD
+// let  obj = "*"
 
-while (obj.length<=7) {
-    console.log(obj);
-    obj+="*";
+// while (obj.length<=7) {
+//     console.log(obj);
+//     obj+="*";
+// }
+
+
+// for (let i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
+
+// function getfullname = (user, age, user2) {
+//     if(age < 18){
+//         console.log("CLOSE")
+//     } else {
+//         console.log
+//     }
+// }
+
+
+
+
+// const user = {
+//     first_name: "john",
+//     last_name: "barbaro"
+// }
+
+// const user2 = {
+//     first_name: "vito",
+//     last_name: "scalette"
+// }
+
+// getfullname(user, 15)
+// getfullname(user, 18)
+
+// const anonym = function (user) {
+//     console.log(user.first_name, user.last_name)
+// }
+
+// anonym();
+
+
+function OBJ(n1,n2) {
+    if (n1 < n2){
+        console.log("True")
+    } 
+    else{
+        console.log("False")
+    }
 }
-
-=======
-
-while (obj.length<=7) {
-    console.log(obj);
-    obj+="*";
+const number = {
+    n1: 50,
+    n2: 100
 }
->>>>>>> f081c8ec9676a1b185f85b3538464a59f1c43703
+OBJ(number.n1, number.n2);
 
-for (let i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+
+
+function array(OBJ1, OBJ2) {
+    if (OBJ1.length > OBJ2.length) {
+        console.log("True")
+    } else {
+        console.log("False")
+    }
 }
+const OBJ1 = ["ncjnc", 3383, true, false];
+const OBJ2 = ["fvss", 113,233,232,32];
+
+array(OBJ1, OBJ2);
+
+
