0af38f35df037de63a6146d2210d012439f8d152
Bekbolsun <linex1827@gmail.com>
2022-04-16 18:55:04 +0600

    dz4

--- greeting.js
+++ greeting.js
@@ -2,11 +2,7 @@ const blocks = document.getElementsByClassName('block')
 // console.log(blocks)
 const block3 = document.getElementById('block3')
 console.log(block3)
-
 block3.classList.add("changedClass")
 console.log(block3.classList)
-
-
-
 block3.classList.remove("newClass")
 console.log(block3.classList)
