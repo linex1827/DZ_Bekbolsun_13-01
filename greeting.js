7cdfcd0bdc64cbc0f415566bfc2f55418dd720de
Bekbolsun <linex1827@gmail.com>
2022-04-16 18:50:22 +0600

    dz44

--- greeting.js
+++ greeting.js
@@ -1,75 +1,11 @@
-// let  obj = "*"
+const blocks = document.getElementsByClassName('block')
+// console.log(blocks)
+const block3 = document.getElementById('block3')
+console.log(block3)
 
-// while (obj.length<=7) {
-//     console.log(obj);
-//     obj+="*";
-// }
-
-
-// for (let i = 1; i < 101; i++) {
-//     if (i % 15 == 0) console.log("FizzBuzz");
-//     else if (i % 3 == 0) console.log("Fizz");
-//     else if (i % 5 == 0) console.log("Buzz");
-//     else console.log(i);
-// }
-
-// function getfullname = (user, age, user2) {
-//     if(age < 18){
-//         console.log("CLOSE")
-//     } else {
-//         console.log
-//     }
-// }
+block3.classList.add("changedClass")
+console.log(block3.classList)
 
 
-
-
-// const user = {
-//     first_name: "john",
-//     last_name: "barbaro"
-// }
-
-// const user2 = {
-//     first_name: "vito",
-//     last_name: "scalette"
-// }
-
-// getfullname(user, 15)
-// getfullname(user, 18)
-
-// const anonym = function (user) {
-//     console.log(user.first_name, user.last_name)
-// }
-
-// anonym();
-
-
-function OBJ(n1,n2) {
-    if (n1 < n2){
-        console.log("True")
-    } 
-    else{
-        console.log("False")
-    }
-}
-const number = {
-    n1: 50,
-    n2: 100
-}
-OBJ(number.n1, number.n2);
-
-
-
-function array(OBJ1, OBJ2) {
-    if (OBJ1.length > OBJ2.length) {
-        console.log("True")
-    } else {
-        console.log("False")
-    }
-}
-const OBJ1 = ["ncjnc", 3383, true, false];
-const OBJ2 = ["fvss", 113,233,232,32];
-
-array(OBJ1, OBJ2);
-
-
+block3.classList.remove("newClass")
+console.log(block3.classList)
